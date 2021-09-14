852eaeae85ce9531d98887911ae37f04664709b2
Tom Ames <tsames1@gmail.com>
2021-09-13 17:09:35 -0700

    Added tip route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+/*********************
+Greetings Routes
+**********************/
 
 app.get('/greeting/', (req, res) => {
   res.send(`Hello, stranger. I\'m not sure, we've met before.`)
@@ -7,6 +10,15 @@ app.get('/greeting/', (req, res) => {
 
 app.get('/greeting/:name', (req, res) => {
   res.send(`Its so nice to meet you ${req.params.name}!`)
+})
+
+/*********************
+Tip Routes
+**********************/
+
+app.get('/tip/:amount/:tipPercentage', (req, res) => {
+  const tip = Number(req.params.amount) * (Number(req.params.tipPercentage) / 100)
+  res.send(`${tip}`)
 })
 
 app.listen(3000, () => {
