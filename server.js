5e3dd46de403e0b6c79fac361aa25cfe9632cb6a
Tom Ames <tsames1@gmail.com>
2021-09-18 12:41:17 -0700

    Added eight ball

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+
 /*********************
 Greetings Routes
 **********************/
@@ -13,13 +14,31 @@ app.get('/greeting/:name', (req, res) => {
 })
 
 /*********************
-Tip Routes
+Tip Route
 **********************/
 
 app.get('/tip/:amount/:tipPercentage', (req, res) => {
   const tip = Number(req.params.amount) * (Number(req.params.tipPercentage) / 100)
   res.send(`${tip}`)
 })
+
+/*********************
+Magic 8 Ball Route
+**********************/
+
+const eightBall = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:question', (req, res) => {
+  const random = Math.floor(Math.random() * eightBall.length - 1);
+  res.send(`
+  <h1>${req.params.question}</h1>
+  <h2>${eightBall[random]}</h2>
+  `)
+})
+
+/*********************
+Listen
+**********************/
 
 app.listen(3000, () => {
 })
